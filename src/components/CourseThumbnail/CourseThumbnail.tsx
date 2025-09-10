198874b27d3988449e2130d804c64d7e189518f5
cheporte <eugene.bevz06@gmail.com>
2025-09-10 16:30:35 +0300

    Made mock dashboard page

---
+++ src/components/CourseThumbnail/CourseThumbnail.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import styles from './CourseThumbnail.module.css';
+import courseCoverImage from '../../assets/img/abstract-watercolor-img.jpg';
+
+type props = {
+  courseTitle: string;
+  progress: number; // progress percentage from 0 to 100
+};
+
+const CourseThumbnail: React.FC<props> = ({ courseTitle, progress }) => {
+  return (
+    <div className={styles.thumbnail} onClick={() => alert(`Navigating to course: ${courseTitle}`)}>
+      <img className={styles.coverImg} src={courseCoverImage} alt="Course Cover" />
+      <div className={styles.courseInfo}>
+        <h3 className={styles.title}>{courseTitle}</h3>
+        <progress value={progress} max={100}></progress>
+      </div>
+    </div>
+  );
+}
+
+export default CourseThumbnail;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Navbar from '../../components/Navbar';
+import Deadlines from '../../components/Deadlines';
+import CourseThumbnail from '../../components/CourseThumbnail';
 
 import styles from './Dashboard.module.css';
 
@@ -8,6 +10,12 @@ const Dashboard: React.FC = () => {
   return (
     <div className={styles.dashboardContainer}>
       <Navbar />
+      <Deadlines />
+      <div className={styles.courseList}>
+        <CourseThumbnail courseTitle={"Організація баз даних"} progress={50}/>
+        <CourseThumbnail courseTitle={"Веб-технології та веб-дизайн"} progress={100}/>
+        <CourseThumbnail courseTitle={"Основи смарт-технологій і систем"} progress={10}/>
+      </div>
     </div>
   );
 }
